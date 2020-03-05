4ff6f68db715d0ccbb11b43ae2823ebd01d9acf8
Mykyta Zavdoviev <m.zavdoviev@aimprosoft.com>
2020-03-05 10:37:40 +0200

    The layout has been ported to Vue.js and the main components are implemented.

---
+++ src/components/Footer.vue
@@ -0,0 +1,18 @@
+<template>
+  <footer class="container">
+    <p class="footer__sidenote">Footer</p>
+  </footer>
+</template>
+
+<script>
+  export default {
+    name: "Footer"
+  }
+</script>
+
+<style scoped lang="scss">
+  .footer__sidenote {
+    font-family: 'Lato-Bold', sans-serif;
+    letter-spacing: 1.37px;
+  }
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,30 @@
+<template>
+  <header class="header container">
+    <h1 class="page-title">{{title}}</h1>
+    <HeaderBag />
+  </header>
+</template>
+<script>
+  import HeaderBag from "./HeaderBag";
+
+  export default {
+    name: 'Header',
+    components: {
+      HeaderBag,
+    },
+    props: {
+      title: String,
+    }
+  }
+</script>
+<style scoped lang="scss">
+  .header {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+  }
+  .page-title {
+    font-size: 20px;
+  }
+
+</style>
